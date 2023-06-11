db294161ee9f04b780f8661467fe825e1963b63a
Puspita <puspitagoswami14@gmail.com>
2023-06-11 23:11:01 +0100

    Add Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {FaBars, FaItems} from 'react-icons/fa';
+import Logo from '../assests/logo.png';
+
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-        
+        <div>
+        <img src={Logo} alt='Logo' style={{width: "80px"}}/>
+        </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;

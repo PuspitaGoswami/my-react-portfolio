d764ce930bfb42a45f108f8f3a5c6f5f2ab2596c
Puspita <puspitagoswami14@gmail.com>
2023-06-11 23:50:29 +0100

    NavItem added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,40 @@
 import React from 'react'
-import {FaBars, FaItems} from 'react-icons/fa';
+import { FaBars, FaItems } from 'react-icons/fa';
 import Logo from '../assests/logo.png';
 
 
 const Navbar = () => {
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
-        <div>
-        <img src={Logo} alt='Logo' style={{width: "80px"}}/>
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div>
+                <img src={Logo} alt='Logo' style={{ width: "80px" }} />
+            </div>
+            <div >
+                <ul className='flex'>
+                    <li>Home</li>
+                    <li>About Me</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div >
+
+            <div className='hidden'>
+                <FaBars />
+            </div>
+            <div className='hidden'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Me</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='hidden'></div>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar;

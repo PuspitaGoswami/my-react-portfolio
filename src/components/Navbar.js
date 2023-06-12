2971ff8da80d17dbfb8161beb23dbab2cebdff14
Puspita <puspitagoswami14@gmail.com>
2023-06-12 23:57:39 +0100

    Mobile View toggle bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaBars, FaItems } from 'react-icons/fa';
 import Logo from '../assests/logo.png';
 
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
             <div>
@@ -19,11 +21,11 @@ const Navbar = () => {
                 </ul>
         
 
-            <div className='md:hidden z-10'>
+            <div onClick={handleClick} className='md:hidden z-10'>
                 <FaBars />
             </div>
             {/* Mobile View */}
-            <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
                 
                     <li className='py-6 text-4xl '>Home</li>
                     <li className='py-6 text-4xl '>About Me</li>

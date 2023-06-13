f15aaad4ee16c54181d2bd877bc896dcb9dfa0ef
Puspita <puspitagoswami14@gmail.com>
2023-06-13 20:42:45 +0100

    Skills section modified

--- src/components/MySkills.js
+++ src/components/MySkills.js
@@ -10,36 +10,36 @@ const MySkills = () => {
   return (
    
     
-        <div name='skills' className='bg-[#0a192f] text-gray-300 py-4'>
+        <div name='skills' className='bg-[#0a192f] text-gray-300 py-4 w-full h-screen'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-auto justify-center w-full h-full'>
                 <div>
-                    <p className='text-4xl border-b-6 border-pink-600 font-bold '>Experience</p>
-                    <p className='py-4'>//There are technologies I've worked with.</p>
+                    <p className='text-4xl inline border-b-4 border-pink-600 font-bold '>Skills</p>
+                    <p className='py-4'>There are technologies I've worked with.</p>
                 </div>
     
-                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 h-fit'>
                
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-fit'>
                         <img className='w-20 mx-auto ' src={html} alt='HTML icon'/>
                         <p className='py-4'>HTML</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-fit'>
                         <img className='w-20 mx-auto ' src={css} alt='CSS icon'/>
                         <p className='py-4'>CSS</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-fit'>
                         <img className='w-20 mx-auto ' src={javascript} alt='Javascript icon'/>
                         <p className='py-4'>Javascript</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-fit'>
                         <img className='w-20 mx-auto ' src={node} alt='Node icon'/>
                         <p className='py-4'>Node.js</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-fit'>
                         <img className='w-20 mx-auto ' src={react} alt='React icon'/>
                         <p className='py-4'>React.js</p>
                     </div>
-                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 h-fit'>
                         <img className='w-20 mx-auto ' src={tailwind} alt='Tailwind icon'/>
                         <p className='py-4'>Tailwind</p>
                     </div>

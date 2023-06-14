4f733a32a5a4aeb1a5abbe838fbc1bb43af74cd4
Puspita <puspitagoswami14@gmail.com>
2023-06-14 23:46:50 +0100

    Hover effect

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,42 @@
 import React from 'react'
-
+import weather from '../assests/weatherApp.JPG';
 
 
 const Projects = () => {
-  return (
-    <div name='projects' className='bg-[#0a192f] w-full md:h-screen text-gray-300 '>
+    return (
+        <div name='projects' className='bg-[#0a192f] w-full md:h-screen text-gray-300 '>
 
-<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-f
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-f
 h-full'>
-    <div>
-        <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Projects</p>
-        <p className='py-6'>Check out some of my works.</p>
-    </div>
-    <div style={{}}
-     className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-        <div className='shadow-lg shadow-[040c16] group contianer round-ms flex justify-center items-center mx-auto content-div '>
-            <span className='text-2xl font-bold text-white tracking-wider'>
-                React.js Application
-            </span>
-            <div className='pt-8 text-center'>
-                <a href='/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                </a>
-                <a href='/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                </a>
+                <div>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Projects</p>
+                    <p className='py-6'>Check out some of my works.</p>
+                </div>
+                <div 
+                    className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                    <div style={{ backgroundImage: `url(${weather})` }} className='shadow-lg shadow-[040c16] group contianer round-ms flex justify-center items-center mx-auto content-div '>
+                        {/* Hover effects */}
+
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React.js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+                                <a href='/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                </a>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-    </div>
-  )
+    )
 }
 
 export default Projects

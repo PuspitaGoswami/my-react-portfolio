a08881d2de083cc7403ec03057c9658d190d5da5
Puspita <puspitagoswami14@gmail.com>
2023-06-15 23:26:52 +0100

    Mobile view menu scroll effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,21 +45,21 @@ const Navbar = () => {
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
 
                 <li className='py-6 text-4xl '>
-                    <Link to="home" smooth={true} duration={500}>
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
                         Home
                     </Link>
                 </li>
-                <li className='py-6 text-4xl '> <Link to="about" smooth={true} duration={500}>
+                <li className='py-6 text-4xl '> <Link onClick={handleClick} to="about" smooth={true} duration={500}>
                         About
                     </Link></li>
-                <li className='py-6 text-4xl '> <Link to="skills" smooth={true} duration={500}>
+                <li className='py-6 text-4xl '> <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
                         Skills
                     </Link></li>
-                <li className='py-6 text-4xl '> <Link to="projects" smooth={true} duration={500}>
+                <li className='py-6 text-4xl '> <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
                         Projects
                     </Link></li>
                 <li className='py-6 text-4xl '>
-                <Link to="contact" smooth={true} duration={500}>
+                <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
                         Contact
                     </Link>
                 </li>

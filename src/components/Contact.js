68b9554dcc89e31bb3894ba0917d63c372b7682e
Puspita <puspitagoswami14@gmail.com>
2023-06-15 18:41:28 +0100

    Inputs and button added on contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,12 @@ const Contact = () => {
         <form action='' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-gray-300 p-4'>Submit the form bellow or send me an email - puspitagoswami14@gmail.com</p>
             </div>
+            <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name' />
+            <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Name' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2' name='message' rows={10} placeholder='Message'/>
+            <button className='text-white border-2 hover:bg-pink-600 border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
   )

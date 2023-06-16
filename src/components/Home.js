91fc23a3b1ee6078f1cf4ca594007388624022cf
Puspita <puspitagoswami14@gmail.com>
2023-06-16 22:58:43 +0100

    Dynamic view works button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi';
+import {Link} from 'react-scroll';
 
 const Home = () => {
+
+
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
         
@@ -13,7 +16,9 @@ const Home = () => {
             I have worked on a variety of projects, from small personal websites to large-scale enterprise applications.
         </p>
         <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work 
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>    <Link to="projects" smooth={true} duration={500}>
+                        View Works
+                    </Link> 
             <span className='group-hover:rotate-90 duration-60'>
             <HiArrowNarrowRight className='ml-3'/>
             </span>
